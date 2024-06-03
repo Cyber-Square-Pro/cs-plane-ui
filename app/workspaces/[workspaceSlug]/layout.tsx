3d256c08b0400716c2919a028d779490081e95e1
adnaaaen <adnanmuhammedadnancp@gmail.com>
2024-06-03 23:34:00 +0530

    style: update workspace ui sticky as needed

--- app/workspaces/[workspaceSlug]/layout.tsx
+++ app/workspaces/[workspaceSlug]/layout.tsx
@@ -9,6 +9,7 @@ import WorkspacePopover from "../_components/workspace-popover";
 import ProfilePopover from "../_components/profile-popover";
 import { UserWrapper } from "./wrapper/user-wrapper";
 
+
 /*
   Author: Reshma on April 21st, 2024
   Purpose: Renders layout for Workspace pages 
@@ -18,6 +19,8 @@ import { UserWrapper } from "./wrapper/user-wrapper";
               - Mohammed Rifad on May 23rd, 2024 - Removed header component to  
                                                    corresponding pages.
               - Sreethu EA on May 23rd, 2024 - Added popover component for workspace and profile 
+              - Muhammed Adnan on May 25th, 2024 - Fixed behavior Sidebar, 
+                                                   Adjusted padding, margin, icon-size as needed.
               - Mohammed Rifad on June 2nd, 2024 - Moved workspace popover and profile popover UI
                 to components folder. Also wrapped layout with UserWrapper.
 */
@@ -34,24 +37,25 @@ const WorkspaceLayout = ({
   return (
     <UserWrapper>
     <div className="min-h-screen flex">
-      <aside className="w-80 border-2 p-2">
+      <aside className="w-[280px] border-2 p-2">
         <nav>
           <div className="flex justify-between mt-1 mr-2">
             <WorkspacePopover />
               <ProfilePopover />
           </div>
-          <div className="flex justify-between mt-4 p-1">
-            <button className="border-2 px-2 py-2 rounded w-64 h-9 flex items-center">
-              <SquarePen size={18} />
-              <span className="text-sm font-medium ml-3">New Issue</span>
+          <div className="flex justify-between mt-3 p-1">
+            <button className="border-2 px-2 py-2 rounded w-64 h-8 flex items-center">
+              <SquarePen size={16} className="text-slate-700"/>
+              <span className="text-[13px] font-medium ml-2">New Issue</span>
             </button>
 
-            <button className="ml-2 border-2 px-2 py-1 rounded w-9 h-9 flex items-center justify-center">
-              <SearchIcon size={18} />
+            <button className="ml-2 border-2 py-1 px-1 rounded w-9 h-8 flex items-center justify-center">
+              <SearchIcon size={16} className="text-slate-700"/>
             </button>
           </div>
-          <div className="w-full cursor-pointer">
+          <div className="w-full cursor-pointer mt-3">
             <SideBar workspaceSlug={workspaceSlug} />
+            {/* <SideBar  /> */}
           </div>
 
           <div className="mt-4 h-full">
@@ -60,8 +64,8 @@ const WorkspaceLayout = ({
         </nav>
       </aside>
 
-      <div className="flex-1 flex flex-col">
-        <main className="pt-16 sm:pt-0 pb-20 h-full">{children}</main>
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <main>{children}</main>
       </div>
     </div>
     </UserWrapper>
