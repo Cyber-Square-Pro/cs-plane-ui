d97a535e30963cd1bb0726a3990889d930a9d2e8
Rifad <140198464+MohammedRifadCyberSquare@users.noreply.github.com>
2024-06-15 16:02:35 +0530

    Revert "feat: added all issues page"

--- store/commad-palette.store.ts
+++ store/commad-palette.store.ts
@@ -12,11 +12,9 @@ export interface ICommandPaletteStore {
 
     isCommandPaletteOpen: boolean;
     isCreateProjectModalOpen: boolean
-    isCreateIssueModalOpen: boolean
 
     // action
     toggleCreateProjectModal: (value?: boolean) => void;
-    toggleCreateIssueModal: (value?: boolean) => void;
 
 }
 
@@ -24,16 +22,13 @@ export class CommandPaletteStore implements ICommandPaletteStore{
 
     isCommandPaletteOpen: boolean = false;
     isCreateProjectModalOpen: boolean = false;
-    isCreateIssueModalOpen: boolean = false;
 
     constructor(_rootStore: RootStore) {
         makeObservable(this, {
             isCommandPaletteOpen: observable.ref,
             isCreateProjectModalOpen: observable.ref,
-            isCreateIssueModalOpen: observable.ref,
 
             toggleCreateProjectModal: action,
-            toggleCreateIssueModal: action,
 
         })
       }
@@ -49,14 +44,5 @@ export class CommandPaletteStore implements ICommandPaletteStore{
         }
         console.log('value',   this.isCreateProjectModalOpen )
       };
-
-      toggleCreateIssueModal = (value?: boolean) => {
-          
-        if (value) {
-          this.isCreateProjectModalOpen = value;
-        } else {
-          this.isCreateProjectModalOpen = !this.isCreateProjectModalOpen;
-        }
-        console.log('value',   this.isCreateProjectModalOpen )
-      };     
+     
 }
